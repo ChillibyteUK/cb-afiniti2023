b60e01738a239cb556d50aee6077216d0e08b9a9
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-09-22 11:31:06 +0100

    cb tabs block

--- inc/cb-blocks.php
+++ inc/cb-blocks.php
@@ -209,6 +209,15 @@ function acf_blocks()
             'mode'	=> 'edit',
             'supports' => array('mode' => false),
         ));
+        acf_register_block_type(array(
+            'name'				=> 'cb_tabs',
+            'title'				=> __('CB Tabs'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/cb_tabs.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
     }
 }
 add_action('acf/init', 'acf_blocks');
