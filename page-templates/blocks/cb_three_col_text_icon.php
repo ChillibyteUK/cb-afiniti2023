f74f5be429eef6e69af557a5ccfc35b85d187729
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-08-29 09:19:20 +0100

    Update cb_three_col_text_icon.php

--- page-templates/blocks/cb_three_col_text_icon.php
+++ page-templates/blocks/cb_three_col_text_icon.php
@@ -5,8 +5,10 @@ $classes = $block['className'] ?? null;
 <div class="container-xl text_icon py-4 <?=$classes?>">
     <div class="row g-4">
         <div class="col-lg-4 d-flex gap-4">
-            <div class="text_icon__icon">
-                <?=wp_get_attachment_image(get_field('col1_icon'), 'medium')?>
+            <div>
+                <div class="text_icon__icon">
+                    <?=wp_get_attachment_image(get_field('col1_icon'), 'medium')?>
+                </div>
             </div>
             <div class="text_icon__text">
                 <h3 class="text--green">
@@ -17,8 +19,10 @@ $classes = $block['className'] ?? null;
             </div>
         </div>
         <div class="col-lg-4 d-flex gap-4">
-            <div class="text_icon__icon">
-                <?=wp_get_attachment_image(get_field('col2_icon'), 'medium')?>
+            <div>
+                <div class="text_icon__icon">
+                    <?=wp_get_attachment_image(get_field('col2_icon'), 'medium')?>
+                </div>
             </div>
             <div class="text_icon__text">
                 <h3 class="text--green">
@@ -29,8 +33,10 @@ $classes = $block['className'] ?? null;
             </div>
         </div>
         <div class="col-lg-4 d-flex gap-4">
-            <div class="text_icon__icon">
-                <?=wp_get_attachment_image(get_field('col3_icon'), 'medium')?>
+            <div>
+                <div class="text_icon__icon">
+                    <?=wp_get_attachment_image(get_field('col3_icon'), 'medium')?>
+                </div>
             </div>
             <div class="text_icon__text">
                 <h3 class="text--green">
