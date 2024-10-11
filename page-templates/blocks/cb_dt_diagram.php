c2a25befabf87a581a99f248c3361c73233d979c
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-10-11 11:50:36 +0100

    dt hub bits

--- page-templates/blocks/cb_dt_diagram.php
+++ page-templates/blocks/cb_dt_diagram.php
@@ -1,36 +1,51 @@
 <section class="dt_diagram py-5">
     <div class="container-xl">
         <div class="row g-2">
-            <div class="col-lg-6 order-lg-2">
+            <div class="col-lg-4 order-lg-2 my-auto">
                 <?=wp_get_attachment_image(get_field('diagram'),'full',null)?>
             </div>
-            <div class="col-lg-3 d-flex flex-column justify-content-around order-lg-1 gap-2">
-                <div class="bg--grey-100 p-2">
-                    <h3 class="text--green">Digital Strategy</h3>
-                    <p><?=get_field('strategy_description')?></p>
-                    <p><em><?=get_field('stragety_strap')?></em></p>
+            <div class="col-lg-4 d-flex flex-column justify-content-around order-lg-1 gap-2">
+                <div class="bg--grey-100 p-2 dt_diagram__card">
+                    <?=wp_get_attachment_image(get_field('strategy_icon'),'large',false,array('class' => 'dt_diagram__icon'))?>
+                    <div class="dt_diagram__inner">
+                        <h3 class="text--green">Digital Strategy</h3>
+                        <p><?=get_field('strategy_description')?></p>
+                        <p><em><?=get_field('strategy_strap')?></em></p>
+                    </div>
                 </div>
-                <div class="bg--grey-100 p-2">
-                    <h3 class="text--dk-purple">Data</h3>
-                    <p><?=get_field('data_description')?></p>
-                    <p><em><?=get_field('data_strap')?></em></p>
+                <div class="bg--grey-100 p-2 dt_diagram__card">
+                    <?=wp_get_attachment_image(get_field('data_icon'),'large',false,array('class' => 'dt_diagram__icon'))?>
+                    <div class="dt_diagram__inner">
+                        <h3 class="text--dk-purple">Data</h3>
+                        <p><?=get_field('data_description')?></p>
+                        <p><em><?=get_field('data_strap')?></em></p>
+                    </div>
                 </div>
             </div>
-            <div class="col-lg-3 d-flex flex-column justify-content-around order-lg-3 gap-2">
-                <div class="bg--grey-100 p-2">
-                    <h3 class="text--purple">People</h3>
-                    <p><?=get_field('people_description')?></p>
-                    <p><em><?=get_field('people_strap')?></em></p>
+            <div class="col-lg-4 d-flex flex-column justify-content-around order-lg-3 gap-2">
+                <div class="bg--grey-100 p-2 dt_diagram__card">
+                    <?=wp_get_attachment_image(get_field('people_icon'),'large',false,array('class' => 'dt_diagram__icon'))?>
+                    <div class="dt_diagram__inner">
+                        <h3 class="text--purple">People</h3>
+                        <p><?=get_field('people_description')?></p>
+                        <p><em><?=get_field('people_strap')?></em></p>
+                    </div>
                 </div>
-                <div class="bg--grey-100 p-2">
-                    <h3 class="text--blue">Process</h3>
-                    <p><?=get_field('process_description')?></p>
-                    <p><em><?=get_field('process_strap')?></em></p>
+                <div class="bg--grey-100 p-2 dt_diagram__card">
+                    <?=wp_get_attachment_image(get_field('process_icon'),'large',false,array('class' => 'dt_diagram__icon'))?>
+                    <div class="dt_diagram__inner">
+                        <h3 class="text--blue">Process</h3>
+                        <p><?=get_field('process_description')?></p>
+                        <p><em><?=get_field('process_strap')?></em></p>
+                    </div>
                 </div>
-                <div class="bg--grey-100 p-2">
-                    <h3 class="text--orange">Systems</h3>
-                    <p><?=get_field('systems_description')?></p>
-                    <p><em><?=get_field('systems_strap')?></em></p>
+                <div class="bg--grey-100 p-2 dt_diagram__card">
+                    <?=wp_get_attachment_image(get_field('system_icon'),'large',false,array('class' => 'dt_diagram__icon'))?>
+                    <div class="dt_diagram__inner">
+                        <h3 class="text--orange">Systems</h3>
+                        <p><?=get_field('system_description')?></p>
+                        <p><em><?=get_field('system_strap')?></em></p>
+                    </div>
                 </div>
             </div>
         </div>
