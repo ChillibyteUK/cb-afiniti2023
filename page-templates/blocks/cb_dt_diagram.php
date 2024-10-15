0a81c931f42cd633858bad263a2a30f4033076d3
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-10-15 09:35:58 +0100

    Update cb_dt_diagram.php

--- page-templates/blocks/cb_dt_diagram.php
+++ page-templates/blocks/cb_dt_diagram.php
@@ -1,16 +1,16 @@
 <section class="dt_diagram py-5">
     <div class="container-xl">
         <div class="row g-2">
-            <div class="col-lg-4 my-auto">
+            <div class="col-lg-5 my-auto text-center">
                 <?=wp_get_attachment_image(get_field('diagram'),'full',null)?>
             </div>
-            <div class="col-lg-8 d-flex flex-column justify-content-around gap-2">
+            <div class="col-lg-7 d-flex flex-column justify-content-around gap-2">
                 <div class="bg--grey-100 p-2 dt_diagram__card">
                     <?=wp_get_attachment_image(get_field('strategy_icon'),'large',false,array('class' => 'dt_diagram__icon'))?>
                     <div class="dt_diagram__inner">
                         <h3 class="text--green">Digital Strategy</h3>
                         <p><?=get_field('strategy_description')?></p>
-                        <p><em><?=get_field('strategy_strap')?></em></p>
+                        <p class="mb-0"><em><?=get_field('strategy_strap')?></em></p>
                     </div>
                 </div>
                 <div class="bg--grey-100 p-2 dt_diagram__card">
@@ -18,7 +18,7 @@
                     <div class="dt_diagram__inner">
                         <h3 class="text--dk-purple">Data</h3>
                         <p><?=get_field('data_description')?></p>
-                        <p><em><?=get_field('data_strap')?></em></p>
+                        <p class="mb-0"><em><?=get_field('data_strap')?></em></p>
                     </div>
                 </div>
                 <div class="bg--grey-100 p-2 dt_diagram__card">
@@ -26,7 +26,7 @@
                     <div class="dt_diagram__inner">
                         <h3 class="text--purple">People</h3>
                         <p><?=get_field('people_description')?></p>
-                        <p><em><?=get_field('people_strap')?></em></p>
+                        <p class="mb-0"><em><?=get_field('people_strap')?></em></p>
                     </div>
                 </div>
                 <div class="bg--grey-100 p-2 dt_diagram__card">
@@ -34,7 +34,7 @@
                     <div class="dt_diagram__inner">
                         <h3 class="text--blue">Process</h3>
                         <p><?=get_field('process_description')?></p>
-                        <p><em><?=get_field('process_strap')?></em></p>
+                        <p class="mb-0"><em><?=get_field('process_strap')?></em></p>
                     </div>
                 </div>
                 <div class="bg--grey-100 p-2 dt_diagram__card">
@@ -42,7 +42,7 @@
                     <div class="dt_diagram__inner">
                         <h3 class="text--orange">Systems</h3>
                         <p><?=get_field('system_description')?></p>
-                        <p><em><?=get_field('system_strap')?></em></p>
+                        <p class="mb-0"><em><?=get_field('system_strap')?></em></p>
                     </div>
                 </div>
             </div>
