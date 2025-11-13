8096301c40e0fac2b2a117b854c150460fd123db
ChillibyteUK <dan@chillibyte.co.uk>
2025-11-13 10:04:33 +0000

    cra additional fields

--- cra-results.php
+++ cra-results.php
@@ -1,66 +1,73 @@
 <?php
+/**
+ * CRA Results Export Script
+ *
+ * This file exports CRA (Change Readiness Assessment) results to CSV format
+ * based on the provided date range filter.
+ */
 
-require_once($_SERVER['DOCUMENT_ROOT'] . "/wp-load.php");
+require_once $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
 
-function array2csv(array &$array)
-{
-   if (count($array) == 0) {
-     return null;
-   }
-   ob_start();
-   $df = fopen("php://output", 'w');
-   fputcsv($df, array_keys(reset($array)));
-   foreach ($array as $row) {
-      fputcsv($df, $row);
-   }
-   fclose($df);
-   return ob_get_clean();
+function array2csv( array &$array ) {
+   	if ( count( $array ) === 0 ) {
+    	return null;
+   	}
+	ob_start();
+	$df = fopen( 'php://output', 'w' );
+	fputcsv( $df, array_keys( reset( $array ) ) );
+	foreach ( $array as $row ) {
+		fputcsv( $df, $row );
+	}
+	fclose( $df );
+	return ob_get_clean();
 }
 
-$q = new WP_Query(array(
-    'post_type' => 'cra',
-    'posts_per_page' => -1,
-    'date_query' => array(
-        array(
-            'after' => $_POST['start'],
-            'before' => $_POST['end'],
-            'inclusive' => true
-        )
-    )
-));
+$q = new WP_Query(
+	array(
+		'post_type'      => 'cra',
+		'posts_per_page' => -1,
+		'date_query'     => array(
+			array(
+				'after'     => $_POST['start'],
+				'before'    => $_POST['end'],
+				'inclusive' => true,
+			),
+		),
+	)
+);
 
-$_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+$_POST = filter_input_array( INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS );
 
-$date = gmdate("Y-m-d\TH:i:s\Z");
+$date     = gmdate( 'Y-m-d\TH:i:s\Z' );
 $filename = 'cra-results_' . $date . '.csv';
 
 ob_start();
 
-// disable caching
-$now = gmdate("D, d M Y H:i:s");
-header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
-header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
-header("Last-Modified: {$now} GMT");
+// disable caching.
+$now = gmdate( 'D, d M Y H:i:s' );
+header( 'Expires: Tue, 03 Jul 2001 06:00:00 GMT' );
+header( 'Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate' );
+header( "Last-Modified: {$now} GMT" );
 
-// force download  
-header("Content-Type: application/force-download");
-header("Content-Type: application/octet-stream");
-header("Content-Type: application/download");
+// force download.
+header( 'Content-Type: application/force-download' );
+header( 'Content-Type: application/octet-stream' );
+header( 'Content-Type: application/download' );
 
-// disposition / encoding on response body
-header("Content-Disposition: attachment;filename={$filename}");
-header("Content-Transfer-Encoding: binary");
+// disposition / encoding on response body.
+header( "Content-Disposition: attachment;filename={$filename}" );
+header( 'Content-Transfer-Encoding: binary' );
 
-$fp = fopen('php://output','w');
+$fp = fopen( 'php://output', 'w' );
 
-$header = array('Report URL','Contact Name','Contact Title','Organisation Name','Contact Phone','Contact Email','Change in Progress?','Change Detail','Change Role','Culture','Leadership','Method','Engagement','Drivers','Capability');
+$header = array( 'Report URL', 'Contact Name', 'Contact Title', 'Organisation Name', 'Contact Phone', 'Contact Email', 'Contact Mobile', 'Contact How Hear', 'Change in Progress?', 'Change Detail', 'Change Role', 'Culture', 'Leadership', 'Method', 'Engagement', 'Drivers', 'Capability' );
 
-fputcsv($fp,$header);
+fputcsv( $fp, $header );
 
-while ($q->have_posts()) {
+while ( $q->have_posts() ) {
     $q->the_post();
-    $data = get_field('data');
-    $scores = get_field('scores');
+    $data   = get_field( 'data' );
+    $scores = get_field( 'scores' );
 
     $results = array();
 
@@ -70,6 +77,8 @@ while ($q->have_posts()) {
     $results[] = $data['orgName'];
     $results[] = "=\"" . $data['contactPhone'] . "\"";
     $results[] = $data['contactEmail'];
+    $results[] = '="' . $data['contactMobile'] . '"';
+    $results[] = $data['contactHowHear'];
     $results[] = $data['changeInProgress'];
     $results[] = $data['changeDetail'];
     $results[] = $data['changeRole'];
@@ -80,7 +89,7 @@ while ($q->have_posts()) {
     $results[] = $scores['Drivers'];
     $results[] = $scores['Capability'];
 
-    fputcsv($fp,$results);
+    fputcsv( $fp, $results );
 }
 
 ob_end_flush();
