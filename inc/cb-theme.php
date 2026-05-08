32836f99417875775ffd27c50ef686761c1a8537
LamcatUK <git@lamcat.co.uk>
2026-05-08 15:52:48 +0100

    no linkedin on people

--- inc/cb-theme.php
+++ inc/cb-theme.php
@@ -16,7 +16,7 @@ require_once CB_THEME_DIR . '/inc/cb-news.php';
 require_once CB_THEME_DIR . '/inc/cb-block-usage.php';
 // require_once CB_THEME_DIR . '/inc/cb-careers.php';
 
-// Remove unwanted SVG filter injection WP
+// Remove unwanted SVG filter injection WP.
 remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
 remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
 
