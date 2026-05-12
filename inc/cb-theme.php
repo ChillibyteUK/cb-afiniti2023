1be044289baab6ae6c166cc8883365b35130441c
ChillibyteUK <dan@chillibyte.co.uk>
2026-05-12 12:59:04 +0100

    contact styling

--- inc/cb-theme.php
+++ inc/cb-theme.php
@@ -328,9 +328,9 @@ add_shortcode('cb_all_people', function () {
 					data-bs-target="#modal-contact-person"
 					data-person-id="<?= esc_attr( get_the_ID() ); ?>"
 					data-person-firstname="<?= esc_attr( $first_name ); ?>"
-					data-person-fullname="<?= esc_attr( $full_name ); ?>">
+					data-person-fullname="<?= esc_attr( $full_name ); ?>"
+					aria-label="Contact <?= esc_attr( $first_name ); ?>">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"/></svg>
-					Contact <?= esc_html( $first_name ); ?>
 				</a>
 				<?php } ?>
 			</div>
