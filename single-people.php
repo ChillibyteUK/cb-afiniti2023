0ea259b4140f5d7a30ca52e5d365cd08b8e1723c
ChillibyteUK <dan@chillibyte.co.uk>
2026-05-13 10:03:01 +0100

    person link

--- single-people.php
+++ single-people.php
@@ -60,7 +60,7 @@ get_header();
 					data-person-fullname="<?= esc_attr( $full_name ); ?>"
 					aria-label="Contact <?= esc_attr( $first_name ); ?>">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false"><path d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"/></svg>
-					<span>Contact <?= esc_html( $first_name ); ?></span<
+					<span>Contact <?= esc_html( $first_name ); ?></span>
 				</a>
 			</div>
 				<?php
